7fe9d66afdffea6de9b3f7538812784159464977
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2022-01-13 18:43:16 +0530

    s

--- src/services/service.service.js
+++ src/services/service.service.js
@@ -125,14 +125,16 @@ const addonservices = (values) => {
   const page = values && values.page;
   const next_page_url = values && values.next_page_url;
   let q = values && values.q ? values.q : "";
-  const action = page ? `afterlogin/categories/view?page=${page}&q=${q}` : `afterlogin/categories/view?q=${q}`;
+  const action = page ? `afterlogin/services/view?page=${page}&q=${q}` : `afterlogin/services/view?q=${q}`;
   const data = {
     auth_key: auth_key,
     action: action,
     salon_id: auth.user.salon_id,
     id: values && values.id ? values.id : "",
-    field: values && values.id ? "" : "name", // first_name,last_name,email
+    field: values && values.id ? "" : "name,isServiceChecked", // first_name,last_name,email
     salon_field: false, //business_name,owner_name
+    serviceprice_field: false,
+    addOnService_field: false,
   };
   return axios.post(next_page_url ? `${next_page_url}&q=${q}` : API_URL + action, data, { headers: authHeader() });
 };
