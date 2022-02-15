3f2f425320b34348471527a0b1f76fd2bc03971b
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2022-02-15 18:58:09 +0530

    updated changes

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
@@ -7,6 +7,9 @@ import Fancybox from "../../component/fancybox.js";
 import config from "../../config";
 // ==============================|| MAIN LAYOUT ||============================== //
 const MainLayout = () => {
+  useEffect(() => {
+   
+  }, []);
   return (
     <>
       <Helmet>
